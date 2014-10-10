2104d70059a7f30e91e987eeff39b886d1266c07
Jim Hayes <jhayes@sdsc.edu>
2014-10-10 13:43:41 -0700

    Incorporate compiler/mpi/network into rpm name for doc purposes.

--- src/vtk/version.mk
+++ src/vtk/version.mk
@@ -1,15 +1,30 @@
-PKGROOT            = /opt/vtk
-NAME               = vtk
-VERSION            = 6.1.0
-RELEASE            = 1
-TARBALL_POSTFIX    = tar.gz
+# Note: normally this package is built with a single compiler, and the rpms
+# from multiple compiler builds will overwrite each other.
 
-SRC_SUBDIR         = vtk
+ifndef ROLLCOMPILER
+  ROLLCOMPILER = gnu
+endif
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
-SOURCE_NAME        = VTK
-SOURCE_VERSION     = $(VERSION)
-SOURCE_SUFFIX      = tar.gz
-SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+ifndef ROLLMPI
+  ROLLMPI = openmpi
+endif
 
-TAR_GZ_PKGS           = $(SOURCE_PKG)
+ifndef ROLLNETWORK
+  ROLLNETWORK = eth
+endif
+
+PKGROOT        = /opt/vtk
+NAME           = vtk_$(COMPILERNAME)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION        = 6.1.0
+RELEASE        = 1
+
+SRC_SUBDIR     = vtk
+
+SOURCE_NAME    = VTK
+SOURCE_SUFFIX  = tar.gz
+SOURCE_VERSION = $(VERSION)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS    = $(SOURCE_PKG)
