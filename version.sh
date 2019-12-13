e62787c57cc28d8ad54776db703c0e1d6dfdce80
Jerry Greenberg <jpg@sdsc.edu>
2019-12-13 15:13:01 -0800

    updated version.sh

--- version.sh
+++ version.sh
@@ -1,24 +1,46 @@
 #!/bin/sh
 
-DESC=`git describe --match 'v*' 2>/dev/null | sed "s/v\([0-9\.]*\)-*\([0-9]*\)-*\([0-9a-z]*\)/\1 \2 \3/"`
+ROCKS_VERSION=`cat /etc/rocks-release 2>/dev/null | awk '{print $3}'`
+DESC_CMD="git describe --match 'v${ROCKS_VERSION}' 2>/dev/null | sed \"s/v\([0-9\.]*\)-*\([0-9]*\)-*\([0-9a-z]*\)/\1 \2 \3/\""
+DESC=`eval ${DESC_CMD}`
+#DESC=`git describe --match 'v*' 2>/dev/null | sed "s/v\([0-9\.]*\)-*\([0-9]*\)-*\([0-9a-z]*\)/\1 \2 \3/"`
 
-if [ -z "${DESC}" ]
+if [ ! -d "./.git" ] && [ -z "${DESC}" ]
 then
     # Try to support using the tagged downloads
-    DESC=`pwd | grep -oe 'vtk-.\+' | sed 's/vtk-//g'`
+    DESC=`pwd | grep -oe 'vtk-roll-.\+' | sed 's/vtk-roll-//g'`
     LOCAL_REV="-github_archive"
 fi
 
 VERSION=`echo ${DESC} | awk '{ print $1 }' | tr "." " "`
 COMMIT=`echo ${DESC} | awk '{ print $2 }'`
 HASH=`echo ${DESC} | awk '{ print $3}'`
+
 VERSION_MAJ=`echo ${VERSION} | awk '{ print $1 }'`
+if [ -z "${VERSION_MAJ}" ]; then
+    VERSION_MAJ="0"
+fi
+
 VERSION_MIN=`echo ${VERSION} | awk '{ print $2 }'`
+if [ -z "${VERSION_MIN}" ]; then
+    VERSION_MIN="0"
+fi
+
+VERSION_DOT=`echo ${VERSION} | awk '{ print $3 }'`
+if [ -n "${VERSION_DOT}" ]; then
+    VERSION_DOT="."${VERSION_DOT}
+fi
+
 VERSION_REV=${COMMIT}
-VERSION_HASH=${HASH}
 if [ -z "${VERSION_REV}" ]; then
     VERSION_REV="0"
 fi
+
+VERSION_HASH=${HASH}
+if [ -z "${VERSION_HASH}" ]; then
+    VERSION_HASH="g"
+fi
+
 
 #Allow local revision identifiers
 #mimicing backports this is "-<identifier>"
@@ -30,11 +52,16 @@ if [ -e localversion ]; then
     fi
 fi
 
-while getopts "vmnrh" opt; do
+while getopts "avmndrh" opt; do
     case $opt in
+	a)
+	   # Major.Minor-Release.Hash
+	   echo "${VERSION_MAJ}.${VERSION_MIN}${VERSION_DOT}-${VERSION_REV}.${VERSION_HASH}"
+	   exit 0
+	   ;;
 	v)
-	   # Major.Minor Version
-	   echo "${VERSION_MAJ}.${VERSION_MIN}"
+	   # Major.Minor.Dot Version
+	   echo "${VERSION_MAJ}.${VERSION_MIN}${VERSION_DOT}"
 	   exit 0
 	   ;;
         m)
@@ -47,6 +74,11 @@ while getopts "vmnrh" opt; do
             echo "${VERSION_MIN}"
             exit 0
             ;;
+	d)
+	    # Dot Version
+	    echo `echo ${VERSION_DOT} | cut -d. -f2`
+	    exit 0
+	    ;;
         r)
             # Revision Version
             echo "${VERSION_REV}"
@@ -67,3 +99,4 @@ done
 VERSION="${VERSION_MAJ}.${VERSION_MIN}.${VERSION_REV}${LOCAL_REV}"
 
 echo $VERSION
+
